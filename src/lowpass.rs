69029f84e28366b846f30890c83d106eac1a70b5
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2023-03-24 16:36:11 +0200

    add alpha check and unsafe methods

--- src/lowpass.rs
+++ src/lowpass.rs
@@ -2,35 +2,92 @@ use std::ops::Neg;
 
 use nalgebra::{RealField, SVector};
 
+macro_rules! assert_alpha {
+    ($alpha:ident, $T:ty) => {
+        for value in $alpha.iter() {
+            assert!(*value >= T::zero());
+            assert!(*value <= T::one());
+        }
+    };
+}
+
 /// Low-pass filter state.
 #[derive(Clone, Copy, Debug)]
 pub struct LowPassFilter<T: RealField, const D: usize> {
-    pub state: SVector<T, D>,
+    state: SVector<T, D>,
 }
 
 impl<T: RealField, const D: usize> LowPassFilter<T, D> {
-    /// Initializes low-pass filter state.
+    /// Initialize low-pass filter state.
     pub fn new(state: SVector<T, D>) -> Self {
         Self { state }
     }
 
-    /// Updates with [`filter`] function using its state as `previous` value.
+    /// Filter new sample (see [`filter`] function) and set result as current state.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `sample` - new signal
+    /// * `alpha` - smoothing factor
+    /// 
+    /// # Panics
+    /// 
+    /// See [`filter`].
     pub fn update(&mut self, sample: &SVector<T, D>, alpha: &SVector<T, D>) {
         self.state = filter(sample, &self.state, alpha);
     }
+
+    /// Same as [`LowPassFilter::update`] but without alpha check.
+    /// 
+    /// # Safety
+    /// 
+    /// See [`filter_unchecked`].
+    pub unsafe fn update_unchecked(&mut self, sample: &SVector<T, D>, alpha: &SVector<T, D>) {
+        self.state = filter_unchecked(sample, &self.state, alpha);
+    }
+
+    /// Current state.
+    pub fn state(&self) -> &SVector<T, D> {
+        &self.state
+    }
 }
 
-/// Filter 1D signal as follows
+impl<T: RealField, const D: usize> AsRef<SVector<T, D>> for LowPassFilter<T, D> {
+    fn as_ref(&self) -> &SVector<T, D> {
+        self.state()
+    }
+}
+
+/// Filter signal as follows
 ///
 ///     current * alpha + (1 - alpha) * previous
 ///
-/// where:
+/// # Arguments
 ///
-/// * `current` - Raw signal
-/// * `previous` - Previous signal
-/// * `alpha` - Smoothing factor
+/// * `current` - raw signal
+/// * `previous` - previous signal
+/// * `alpha` - smoothing factor
+/// 
+/// # Panics
+/// 
+/// This function will panic if any value in `alpha` is out of \[0, 1\] range.
 #[inline]
 pub fn filter<T: RealField, const D: usize>(
+    current: &SVector<T, D>,
+    previous: &SVector<T, D>,
+    alpha: &SVector<T, D>,
+) -> SVector<T, D> {
+    assert_alpha!(alpha, T);
+    unsafe { filter_unchecked(current, previous, alpha) }
+}
+
+/// Same as [`filter`] but without smoothing factor (`alpha`) check.
+/// 
+/// # Safety
+/// 
+/// Each value in `alpha` should be in \[0, 1\] range.
+#[inline]
+pub unsafe fn filter_unchecked<T: RealField, const D: usize>(
     current: &SVector<T, D>,
     previous: &SVector<T, D>,
     alpha: &SVector<T, D>,
