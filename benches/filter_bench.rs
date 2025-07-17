848f04a91eaf9c25e62448c53e19a72eb498cec7
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2025-07-17 17:18:01 +0300

    refactor: use `black_box` from `std`

--- benches/filter_bench.rs
+++ benches/filter_bench.rs
@@ -1,4 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
+use std::hint::black_box;
+
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
 use nalgebra::SVector;
 use one_euro::{OneEuroFilter, OneEuroState};
 
